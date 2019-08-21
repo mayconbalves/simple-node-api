e7a2840d8e9f17b895fd4e4497d1f29b2d9893dc
mayconbalves <mayconmustaine@hotmail.com>
2019-08-20 22:24:30 -0300

    feat(routes): create post router

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+const product = require('./routes/product.router') // Imports routes for the products
+
+app.use('/products', product)
 
 const port = 8080
 
--- routes/product.router.js
+++ routes/product.router.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
-const product_controller = require('../controllers/product.controller');
+const product_controller = require('../controllers/product.controller')
 
 
 // a simple test url to check that all of our files are communicating correctly.
-router.get('/test', product_controller.test);
-module.exports = router;
+router.get('/test', product_controller.test)
+router.post('/create', product_controller.product_create)
+module.exports = router
