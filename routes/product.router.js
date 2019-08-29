b6d7d21bd7d24c866f74921f94c46de66fde49d8
mayconbalves <mayconmustaine@hotmail.com>
2019-08-28 22:28:17 -0300

    feat(routes): create new router for product id

--- routes/product.router.js
+++ routes/product.router.js
@@ -7,5 +7,6 @@ const product_controller = require('../controllers/product.controller')
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', product_controller.test)
+router.get('/:id', product_controller.product_details)
 router.post('/create', product_controller.product_create)
 module.exports = router
