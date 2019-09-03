5a25f5ca32020e15cfa6739b8a44e046eb1eb11d
mayconbalves <mayconmustaine@hotmail.com>
2019-09-02 22:31:28 -0300

    feat(routes): create delete router

--- routes/product.router.js
+++ routes/product.router.js
@@ -10,4 +10,6 @@ router.get('/test', product_controller.test)
 router.get('/:id', product_controller.product_details)
 router.post('/create', product_controller.product_create)
 router.put('/:id/update', product_controller.product_update)
+router.delete('/:id/delete', product_controller.product_delete)
+
 module.exports = router
